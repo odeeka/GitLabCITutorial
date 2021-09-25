dbd10984e7c225fd6d42d25b934411bcd2e5d274
odeeka <ptibor@netbuild.hu>
2021-09-25 20:33:15 +0200

    Update .build_script.sh

--- .build_script.sh
+++ .build_script.sh
@@ -2,12 +2,13 @@
 #update your token and git url in the clone command below
 #update your s3 bucket name
 
- git clone -b develop  https://gitlab+deploy-token-497024:$TOKEN@gitlab.com/piyush.sachdeva055/myfirstproject.git
- cd myfirstproject
+ #git clone -b develop  https://gitlab+deploy-token-497024:$TOKEN@gitlab.com/piyush.sachdeva055/myfirstproject.git
+ git clone -b develop https://github.com/odeeka/GitLabCITutorial.git
+ cd GitLabCITutorial
  npm install express --save
  cd ../
  apt-get update -y
  apt-get install zip -y
  apt-get install awscli -y
  zip -r myfirstproject.zip myfirstproject/ -x '*/node_modules/*' -x '*/.git/*'
- sudo aws s3 myfirstproject.zip s3://$S3_BUCKET_NAME/
+ #sudo aws s3 myfirstproject.zip s3://$S3_BUCKET_NAME/
