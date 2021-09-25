b9bed94a62c8e3faae4126112ace909f6a16cad3
odeeka <ptibor@netbuild.hu>
2021-09-25 20:36:22 +0200

    Update .build_script.sh

--- .build_script.sh
+++ .build_script.sh
@@ -5,10 +5,12 @@
  #git clone -b develop  https://gitlab+deploy-token-497024:$TOKEN@gitlab.com/piyush.sachdeva055/myfirstproject.git
  git clone -b develop https://github.com/odeeka/GitLabCITutorial.git
  cd GitLabCITutorial
+ ls -al
  npm install express --save
+ ls -al
  cd ../
  apt-get update -y
  apt-get install zip -y
  apt-get install awscli -y
- zip -r myfirstproject.zip myfirstproject/ -x '*/node_modules/*' -x '*/.git/*'
+ zip -r gitlabcitutorial.zip GitLabCITutorial/ -x '*/node_modules/*' -x '*/.git/*'
  #sudo aws s3 myfirstproject.zip s3://$S3_BUCKET_NAME/
